8978a03917527d2b3c2ef9fd90aa01c26268c33b
mohammed-najib <mohammed.njeeb@gmail.com>
2022-08-03 23:04:32 +0300

    init

---
+++ helper-hardhat-config.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:
+            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "9558",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    42: {
+        name: "kovan",
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        wethToken: "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
+        lendingPoolAddressesProvider:
+            "0x88757f2f99175387aB4C6a4b3067c77A695b0349",
+        daiEthPriceFeed: "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541",
+        daiToken: "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:
+            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
